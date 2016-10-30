6ac508376d5f2f55c44353667540d5ae7eaac303
Danny Huynh <danhuynh0803@gmail.com>
2016-10-30 16:26:16 -0400

    Added B1 raycast controls to move objects

---
+++ Assets/DirectorController.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DirectorController : MonoBehaviour {
+
+	public Camera camera;
+	public float pushForce; 
+
+	List<GameObject> obstacles = new List<GameObject> ();
+
+	private bool obstacleSelected = false;
+
+	// Use this for initialization
+	void Start () {
+		ResetObstacles ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//ResumePathing ();
+
+		RaycastHit hit;
+		Ray ray = camera.ScreenPointToRay (Input.mousePosition);
+
+		if (Input.GetButtonDown ("Fire1")) { 
+			if (Physics.Raycast (ray, out hit)) {
+				if (hit.collider.gameObject.tag == "Obstacle") {
+					ResetObstacles ();
+					obstacles.Add (hit.collider.gameObject); // Add selected obstacles to obstacles list
+					hit.collider.gameObject.GetComponent<ObstacleController>().isSelected = true;
+					camera.gameObject.GetComponent<FreeLookCamera>().canMove = false;
+					obstacleSelected = true;
+				}
+				
+			}
+		} else if (Input.GetButtonDown ("Fire2")) { // Deselect everything
+			ResetObstacles (); 
+		}
+
+		if (obstacleSelected) {
+			camera.gameObject.GetComponent<FreeLookCamera> ().canMove = false;			
+			float horizontal = Input.GetAxis ("Horizontal");
+			float vertical = Input.GetAxis ("Vertical");
+			obstacles[0].GetComponent<Transform> ().Translate (-pushForce * new Vector3 (horizontal, 0, vertical) * Time.deltaTime);
+		} else {
+			camera.gameObject.GetComponent<FreeLookCamera> ().canMove = true;
+			ResetObstacles ();
+		}
+
+		
+	}
+
+	void ResetObstacles()
+	{
+		foreach (GameObject go in obstacles) 
+		{
+			go.GetComponent<ObstacleController> ().isSelected = false;
+		}
+		camera.gameObject.GetComponent<FreeLookCamera> ().canMove = true;
+		obstacleSelected = false;
+		obstacles.Clear ();
+	}
+
+}
---
+++ Assets/ObstacleController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstacleController : MonoBehaviour {
+
+	public bool isSelected; 
+
+	private Rigidbody rigidbody;
+	private Color setColor;
+	void Start () {
+		setColor = this.GetComponent<Renderer> ().material.color;
+		rigidbody = GetComponent<Rigidbody> ();
+		isSelected = false;
+	}
+	
+	void Update () {
+		if (this.isSelected) { 		// Change color of agent to orange represent selection
+			this.GetComponent<Renderer> ().material.color = new Color (0.9f, 0.5f, 0.3f);
+		} else { 					
+			this.GetComponent<Renderer> ().material.color = setColor;  // Change color back to original material color;
+		}					
+			
+
+	}
+}
