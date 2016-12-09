625632a7dacd51f93e85bbf97d0ce693ca630d93
Ryan Parisen <flippermetimbers@gmail.com>
2016-12-08 20:08:06 -0500

    added start screen and controller

---
+++ Assets/VideoGame Assets/Scripts/Controller.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Controller : MonoBehaviour
+{
+    
+    private bool GemCollected = false;
+
+    void Start()
+    {
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Gem"))
+            {
+                GemCollected = true;
+                other.gameObject.SetActive(false);
+            }
+    }
+
+}
---
+++ Assets/VideoGame Assets/Scripts/StartButton.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour {
+
+    void Start()
+    {
+        Pause();
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void UnPause()
+    {
+        Time.timeScale = 1;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
