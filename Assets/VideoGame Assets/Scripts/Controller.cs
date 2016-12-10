cba07144c53539e820ecd4f5295604758f9432a3
Ryan Parisen <flippermetimbers@gmail.com>
2016-12-10 14:30:54 -0500

    made prelim design of compound the player must break into

--- Assets/VideoGame Assets/Scripts/Controller.cs
+++ Assets/VideoGame Assets/Scripts/Controller.cs
@@ -6,6 +6,9 @@ public class Controller : MonoBehaviour
 {
     public Text WinText;
     private bool GemCollected = false;
+    private bool Key1Collected = false;
+    private bool Key2Collected = false;
+    private bool Key3Collected = false;
 
     void Start()
     {
@@ -18,6 +21,24 @@ public class Controller : MonoBehaviour
                 GemCollected = true;
                 other.gameObject.SetActive(false);
             }
+
+        if (other.gameObject.CompareTag("Key1"))
+        {
+            Key1Collected = true;
+            other.gameObject.SetActive(false);
+        }
+
+        if (other.gameObject.CompareTag("Key2"))
+        {
+            Key2Collected = true;
+            other.gameObject.SetActive(false);
+        }
+
+        if (other.gameObject.CompareTag("Key3"))
+        {
+            Key3Collected = true;
+            other.gameObject.SetActive(false);
+        }
     }
 
     void OnCollisionEnter(Collision other)
@@ -29,6 +50,14 @@ public class Controller : MonoBehaviour
                     winGame();
                 }
             }
+
+        if (other.gameObject.CompareTag("Vault"))
+        {
+            if (Key1Collected && Key2Collected && Key3Collected)
+            {
+                other.gameObject.SetActive(false);
+            }
+        }
     }
 
     void winGame()
--- Assets/VideoGame Assets/Scripts/VideoGameBT.cs
+++ Assets/VideoGame Assets/Scripts/VideoGameBT.cs
@@ -1,15 +1,29 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
+using TreeSharpPlus;
 
-public class VideoGameBT : MonoBehaviour {
+public class VideoGameBT : MonoBehaviour
+{
+    public Transform point1;
+    public Transform point2;
+    public Transform point3;
+    public Transform point4;
+    public GameObject Guard;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    private BehaviorAgent bAgent;
+    // Use this for initialization
+    void Start()
+    {
+        //bAgent = new BehaviorAgent(this.BuildTreeRoot());
+        //BehaviorManager.Instance.Register(bAgent);
+        //bAgent.StartBehavior();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
